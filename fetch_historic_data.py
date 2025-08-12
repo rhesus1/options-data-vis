476b4f7161f566017ca30c060ebd00691479b946
Morgan Rees <rhesus1@users.noreply.github.com>
2025-08-12 17:44:20 +0100

    Update fetch_historic_data.py

--- fetch_historic_data.py
+++ fetch_historic_data.py
@@ -14,7 +14,11 @@ def fetch_historic_data(ticker):
         return pd.DataFrame()
     hist = hist[['Close']]
     hist['Log_Return'] = np.log(hist['Close'] / hist['Close'].shift(1))
-    hist['Realized_Vol'] = hist['Log_Return'].rolling(window=90).std() * np.sqrt(252) * 100  # Annualized volatility in percent
+    hist['Realized_Vol_30'] = hist['Log_Return'].rolling(window=30).std() * np.sqrt(252) * 100
+    hist['Realized_Vol_60'] = hist['Log_Return'].rolling(window=60).std() * np.sqrt(252) * 100
+    hist['Realized_Vol_90'] = hist['Log_Return'].rolling(window=90).std() * np.sqrt(252) * 100
+    hist['Realized_Vol_180'] = hist['Log_Return'].rolling(window=180).std() * np.sqrt(252) * 100
+    hist['Realized_Vol_360'] = hist['Log_Return'].rolling(window=360).std() * np.sqrt(252) * 100
     hist = hist.dropna()
     hist['Date'] = hist.index.strftime('%Y-%m-%d')
     hist['Ticker'] = ticker
