b5a710e15efeb3c819fe47e9f5f5d41070a111bd
Morgan Rees <rhesus1@users.noreply.github.com>
2025-08-12 17:39:53 +0100

    Update clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -1,5 +1,5 @@
-# clean_data.py
 import pandas as pd
+import numpy as np
 import glob
 import os
 
@@ -10,47 +10,32 @@ def main():
         return
     latest_raw = max(raw_files, key=os.path.getctime)
     df = pd.read_csv(latest_raw, parse_dates=['Expiry'])
-
     duplicates = df.duplicated(subset=['Contract Name']).sum()
     if duplicates > 0:
         df = df.drop_duplicates(subset=['Contract Name'])
-
-    #key_cols = ['Ticker', 'Type', 'Strike', 'Moneyness', 'Bid', 'Ask', 'Volume', 
-    #            'Open Interest', 'Last Option Price', 'Implied Volatility', 'Last Stock Price']
-    #na_before = df[key_cols].isna().any(axis=1).sum()
-    #df = df.dropna(subset=key_cols)
-
+    
+    # Set Implied Volatility to NaN if < 0% or > 300%
+    df.loc[(df['Implied Volatility'] < 0) | (df['Implied Volatility'] > 3.0), 'Implied Volatility'] = np.nan
+    
+    # Cap Moneyness between 0% and 350%
+    df = df[(df['Moneyness'] >= 0) & (df['Moneyness'] <= 3.5)]
+    
     df = df[
         (df['Volume'] >= 0) &
         (df['Open Interest'] >= 0) &
         (df['Bid'] >= 0) &
         (df['Ask'] >= 0) &
-        #(df['Moneyness'] <= 300) &
-        #(df['Moneyness'] >= 10) &
-        #(df['Bid'] <= df['Ask']) &  # Strict < to ensure positive spread
-        #(df['Bid'] <= df['Last Option Price']) &
-        #(df['Last Option Price'] <= df['Ask']) &
-        (df['Implied Volatility'] > 0.001)  # Remove zero IV
+        (df['Implied Volatility'] > 0.001)  # Remove zero IV (after NaN handling)
     ]
-    
+   
     volume_threshold = df['Volume'].quantile(0.25)
     oi_threshold = df['Open Interest'].quantile(0.25)
-    
+   
     df = df[
         (df['Volume'] >= volume_threshold) &
         (df['Open Interest'] >= oi_threshold)
     ]
-
-    #df['Spread'] = df['Ask'] - df['Bid']
-    #df['Mid Price'] = (df['Ask'] + df['Bid']) / 2
-    #df = df[df['Spread'] / df['Mid Price'] <= 0.2]
-
-    #for col in ['Volume', 'Open Interest', 'Implied Volatility']:
-    #    Q1 = df[col].quantile(0.25)
-    #    Q3 = df[col].quantile(0.75)
-    #    IQR = Q3 - Q1
-    #    df = df[(df[col] >= Q1 - 1.5 * IQR) & (df[col] <= Q3 + 1.5 * IQR)]
-    
+   
     timestamp = os.path.basename(latest_raw).split('raw_')[1].split('.csv')[0]
     clean_filename = f'data/cleaned_{timestamp}.csv'
     df.to_csv(clean_filename, index=False)
