9a859e0825256066f02a64248232fe2b324a4c66
Morgan Rees <rhesus1@users.noreply.github.com>
2025-08-14 07:54:17 +0100

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -246,9 +246,9 @@ def fetch_historic_data(ticker):
     hist['Log_Return'] = np.log(hist['Close'] / hist['Close'].shift(1))
     hist['Realised_Vol_30'] = hist['Log_Return'].rolling(window=30).std() * np.sqrt(252) * 100
     hist['Realised_Vol_60'] = hist['Log_Return'].rolling(window=60).std() * np.sqrt(252) * 100
-    hist['Realised_Vol_90'] = hist['Log_Return'].rolling(window=90).std() * np.sqrt(252) * 100
+    hist['Realised_Vol_100'] = hist['Log_Return'].rolling(window=100).std() * np.sqrt(252) * 100
     hist['Realised_Vol_180'] = hist['Log_Return'].rolling(window=180).std() * np.sqrt(252) * 100
-    hist['Realised_Vol_360'] = hist['Log_Return'].rolling(window=360).std() * np.sqrt(252) * 100
+    hist['Realised_Vol_252'] = hist['Log_Return'].rolling(window=252).std() * np.sqrt(252) * 100
     hist = hist.dropna()
     hist['Date'] = hist.index.strftime('%Y-%m-%d')
     hist['Ticker'] = ticker
