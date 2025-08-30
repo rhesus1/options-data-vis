411f0e7ce903ce21cde392ca6e21339179273d25
Morgan Rees <rhesus1@users.noreply.github.com>
2025-08-30 12:33:31 +0100

    Update Force_Calculate_Ranking.py

--- Force_Calculate_Ranking.py
+++ Force_Calculate_Ranking.py
@@ -3,7 +3,6 @@ from calculate_ranking_metrics import calculate_ranking_metrics
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python Force_Calculate_Ranking.py <timestamp>")
         sys.exit(1)
     
     timestamp = sys.argv[1]
