0e016297f78dfaa3ace44ac11a25fc7f7130e68d
Morgan Rees <rhesus1@users.noreply.github.com>
2025-09-01 22:37:18 +0100

    Update Force_Calculate_Ranking.py

--- Force_Calculate_Ranking.py
+++ Force_Calculate_Ranking.py
@@ -1,14 +1,13 @@
 import sys
-
 from calculate_ranking_metrics import calculate_ranking_metrics
 
 def main():
     if len(sys.argv) < 2:
         print("Usage: python Force_Calculate_Ranking.py <timestamp>")
         sys.exit(1)
-    
+   
     timestamp = sys.argv[1]
-    sources = ['yfinance', 'nasdaq']
+    sources = ['yfinance']  # Updated to only include 'yfinance'
     calculate_ranking_metrics(timestamp, sources)
 
 main()
