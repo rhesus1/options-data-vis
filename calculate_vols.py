b807ea56c262d2c37ed919fa6285b661742cf4da
Morgan Rees <rhesus1@users.noreply.github.com>
2025-09-17 12:25:43 +0100

    Update calculate_vols.py

--- calculate_vols.py
+++ calculate_vols.py
@@ -575,8 +575,6 @@ def process_volumes(timestamp):
         df = calculate_smoothed_iv(df, params_calls, params_puts, model)
         df['TotalVariance'] = df['Smoothed_IV']**2 * df['Years_to_Expiry']
         df['TotalVariance'] = df['TotalVariance'].fillna(np.nan)
-
-        skew_metrics_df, slope_metrics_df = calculate_skew_slope_metrics(df, ticker, timestamp, r=df['r'].iloc[0], q=q)
         
         output_columns = [
             'Ticker', 'Contract Name', 'Type', 'Expiry', 'Strike', 'Moneyness', 'Bid', 'Ask', 'Volume', 'Open Interest',
@@ -627,6 +625,9 @@ def process_volumes(timestamp):
                     print(f"Error reading existing {vol_surf_file}: {e}")
             param_df.to_csv(vol_surf_file, index=False)
             print(f"Volatility surface parameters saved to {vol_surf_file}")
+
+        skew_metrics_df, slope_metrics_df = calculate_skew_slope_metrics(df, ticker, timestamp, r=df['r'].iloc[0], q=q)
+
     
     dates_file = 'data/dates.json'
     if os.path.exists(dates_file):
